8d99f58b0f0d6eb038896cb37a959d0b9dd065bb
CodeOne45 <malikaman1999@gmail.com>
2023-11-27 18:00:42 +0100

    feat: added ingress

---
+++ part 2 - GKE - K8s/k8s-ingress.tf
@@ -0,0 +1,31 @@
+resource "kubernetes_ingress_v1" "vote-ingess" {
+  metadata {
+    name = "vote-ingress"
+  }
+  spec {
+    default_backend {
+      service {
+        name = "vote"
+        port {
+          number = 5000
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_ingress_v1" "result-ingess" {
+  metadata {
+    name = "result-ingress"
+  }
+  spec {
+    default_backend {
+      service {
+        name = "result"
+        port {
+          number = 5001
+        }
+      }
+    }
+  }
+}
